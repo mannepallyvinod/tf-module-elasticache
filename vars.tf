47ea2f5ee02fcb7dec934fc589054216df8ae843
mannepallyvinod <111588987+mannepallyvinod@users.noreply.github.com>
2023-04-25 11:53:46 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "tags" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "num_cache_nodes" {}
+variable "node_type" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "allow_subnets" {}
